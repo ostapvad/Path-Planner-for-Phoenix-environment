f3d37e6582cfc30ad50e3d2638af3725e5f4ebed
ostapvad <dm.mix.poc@gmail.com>
2021-04-04 05:52:00 +0200

    models are done, building the world

---
+++ gazebo_path_plugin/src/build_world.cpp
@@ -0,0 +1,17 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "gazebo_msgs/SpawnModel.h" 
+#include <iostream>
+
+void spawn_pslots();
+
+
+int main(int argc, char **argv){
+    //ros::NodeHandle n;
+    spawn_pslots();
+    return 0;
+ }
+
+void spawn_pslots(){
+    printf("wow!\n");
+}
