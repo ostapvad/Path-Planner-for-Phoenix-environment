e8463dcd9a42686614350ef60f3ed56da2b6b5fc
ostapvad <dm.mix.poc@gmail.com>
2021-04-07 16:58:36 +0200

    added readme

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Path-Planner-for-Phoenix-environment
-Update Info folder for further work
+How to run the plugin
+
+1) Clone the repository.
+2) Copy the folder gazebo_path_plugin to your workspace/src and catkin_make.
+3) Update your .bashrc file, paste at the end, change the path to your workspace.
+	source /home/ostapvad/Documents/Gazebo_ws/devel/setup.bash # Path to your workspace
+	export GAZEBO_PLUGIN_PATH=~/Documents/Gazebo_ws/devel/lib/ # Path to the same workspace for Gazebo plugins
+	export GAZEBO_MODEL_PATH=/home/ostapvad/Documents/Gazebo_ws/src/gazebo_path_plugin/models:$GAZEBO_MODEL_PATH
+4) cd to Shell/, update the line in run_mypark.sh to your workspace
+ 	ws_path="/home/ostapvad/Documents/Gazebo_ws" # path to ROS workpace CHANGE TO YOUR CUSTOM
+5) run with ./run_mypark.sh(for tmux users). Otherwise go to your worspace and run in separate terminal the commands:
+	roscore
+	rosrun gazebo_ros gazebo -u
+	rosrun gazebo_ros gazebo -u
+
+6)Other Shell files were written for parking branch in Veronika's repository, you can clone the required repositeries and install this branch with ./init_sim_ws, don't forget to change 
+	# Change to whereever you want
+	ws_folder_path="/home/ostapvad/Documents/"
+	folder_name="Phoenix_ws"
+7)      ./run_parking_sim.sh - simulation in the parking mode
+	./run_static_sim.sh  - simulation in the static mode
+
+	Change in all shell files path to your workspace
--- gazebo_path_plugin/src/build_world.cpp
+++ gazebo_path_plugin/src/build_world.cpp
@@ -1,17 +1,71 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "std_msgs/Float64.h"
 #include "gazebo_msgs/SpawnModel.h" 
 #include <iostream>
+#include <fstream>
 
-void spawn_pslots();
+int w_s = 4;    //[m] - width of moving path for the steerbot
+int w_p = 2.5;  //[m] - width of parking slot
+int l_p = 5.5;  //[m] - length of parking slot
+int N = 10;     //[m] - number of parking slots in one row
+std::string path_to_file = "/home/ostapvad/Documents/Gazebo_ws/src/gazebo_path_plugin/models/parking_slot/model.sdf"; // path to model
+
+std::string readFileIntoString2(const std::string & path); // reads file into string
+//void spawn_pslots();
+//void call_spawn(const std_msgs::String::ConstPtr& model_name, int px, int py);
 
 
 int main(int argc, char **argv){
-    //ros::NodeHandle n;
-    spawn_pslots();
+    ros::init(argc, argv, "build_world_client");
+    if(argc > 1){
+        ROS_INFO("Incorrect usage of build_world. Return error");
+        return 1;
+    }
+    ros::NodeHandle n;
+
+    ros::ServiceClient client = n.serviceClient<gazebo_msgs::SpawnModel>("builder");
+    gazebo_msgs::SpawnModel srv;
+    std::string model_name = "parking_slot";
+   
+    //std::string num_str1(std::to_string(1.25));
+    //num_str1.append("chuj");
+    
+    std::string xml_data = readFileIntoString2(path_to_file);
+  
+    std_msgs::Float64 px, py;
+    for (int i = 0; i < N; i++){
+        px.data = w_s/2 + w_p*(N- i + 0.5);
+        py.data = w_s/2 + l_p/2;
+        //srv.request.model_name = "";
+        srv.request.model_xml = xml_data;
+        srv.request.robot_namespace = "-file";
+        //srv.request.reference_frame = "";
+        srv.request.initial_pose.position.x = px.data;
+        srv.request.initial_pose.position.y = py.data;
+        //ROS_INFO("5s", srv.request);
+        if(client.call(srv)){ 
+            ROS_INFO("Spawned model: %s", srv.response.status_message);
+        }
+        else {
+            ROS_ERROR("Failed to call service");
+            return 1;
+        }
+    }
+    
     return 0;
  }
 
-void spawn_pslots(){
-    printf("wow!\n");
+
+std::string readFileIntoString2(const std::string & path) {
+    auto ss = std::ostringstream{};
+    std::ifstream input_file(path);
+    if (!input_file.is_open()) {
+        std::cerr << "Could not open the file - '"
+             << path << "'" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    ss << input_file.rdbuf();
+    return ss.str();
 }
+
